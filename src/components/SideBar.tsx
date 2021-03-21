af0d4decefefe39f1214c655826873efce0e628f
Michel <michel.i@gmail.com>
2021-03-20 23:00:30 -0300

    Alteracoes do desafio 02

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import '../styles/sidebar.scss';
+
+import { Button } from './Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBar {
+  genres: Array<GenreResponseProps>;
+  selectedGenre: number;
+  clicking: (id: number) =>void;
+}
+
+  export function SideBar({genres,selectedGenre,clicking}:SideBar) {
+
+  return(
+      <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={()=>clicking(genre.id)}
+            selected={selectedGenre === genre.id}
+          />
+        ))}
+      </div>
+        
+    </nav>
+  );
 }
